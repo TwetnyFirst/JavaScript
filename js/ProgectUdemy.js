6845e107b4c13faea8de73a50d6129555441a136
Aleksandr Cilindz <aleksandr@macbook-air-aleksandr.home>
2023-05-25 10:10:31 +0200

    modificant first progect by add two cycles

--- js/ProgectUdemy.js
+++ js/ProgectUdemy.js
@@ -8,8 +8,30 @@ const PersonalMoviesDB = {
     privat:false
 
 }
-const film1 = prompt("one of the last films you watched?"),
-    b = prompt("Grade?");
-PersonalMoviesDB.movies[film1] = b;
+
+
+
+
+for(let x = 0;x < 2;x++ ){
+    let film = prompt("one of the last films you watched?"),
+        a= prompt("Grade?");
+
+    if(film != null && a != null && film != '' && a != '' && a.length < 50){
+        PersonalMoviesDB.movies[film] = a;
+    }else{
+        console.log("idi hanij");
+        x--;
+    }
+
+}
+if(PersonalMoviesDB.count < 10){
+    console.log("pizdet ty tupoj");
+
+}else if(PersonalMoviesDB.count >= 10 && PersonalMoviesDB.count <= 30){
+    console.log("Ny normalno");
+
+}else{
+    console.log("Ebat ty krasavczik");
+}
 
 console.log(PersonalMoviesDB);
--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-let arr = [1,2,3,4];
-arr[10] = 5;
-console.log(arr[9]);
-console.log("hello");
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+
+console.log(result)
